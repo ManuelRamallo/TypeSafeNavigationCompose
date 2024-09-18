a5a54b16819e8ed51d36d83a0c778b4fe243501f
Manuel Ramallo Díaz <32164753+manuelramallo@users.noreply.github.com>
2024-09-18 19:56:48 +0200

    Implementation libraries and example for typeSafe Navigation

--- app/src/main/java/com/mramallo/typesafenavigationcompose/MainActivity.kt
+++ app/src/main/java/com/mramallo/typesafenavigationcompose/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.mramallo.typesafenavigationcompose.ui.theme.TypeSafeNavigationComposeTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +24,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TypeSafeNavigationComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = ScreenA
+                ) {
+                    composable<ScreenA> {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            Button(onClick = {
+                                navController.navigate(
+                                    ScreenB(
+                                        name = null,
+                                        age = 25
+                                    )
+                                )
+                            }) {
+                                Text(text = "Go to Screen B")
+                            }
+                        }
+                    }
+                    composable<ScreenB> {
+                        val args = it.toRoute<ScreenB>()
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            Text(text = "${args.name}, ${args.age} years old")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+@Serializable
+object ScreenA
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TypeSafeNavigationComposeTheme {
-        Greeting("Android")
-    }
-}
+@Serializable
+data class ScreenB(
+    val name: String?,
+    val age: Int
+)
